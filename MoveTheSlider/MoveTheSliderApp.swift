bd5439547d79f5b1971179c44168c9d153b8a388
Aleksey Efimov <smaylik121@gmail.com>
2023-05-14 22:25:21 +0300

    Initial Commit

---
+++ MoveTheSlider/MoveTheSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveTheSliderApp.swift
+//  MoveTheSlider
+//
+//  Created by Aleksey Efimov on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoveTheSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
